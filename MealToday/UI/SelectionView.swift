9d0e8f8f94f59ff0637928ab0ced7e83c9b2597e
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2021-05-26 23:21:45 +0900

    [#12] Update SelectionView.swift

--- MealToday/UI/SelectionView.swift
+++ MealToday/UI/SelectionView.swift
@@ -8,34 +8,40 @@
 import SwiftUI
 
 struct SelectionView: View {
-    @EnvironmentObject var appState: AppState
+    @ObservedObject var viewModel: ViewModel = ViewModel()
 
     var body: some View {
         VStack {
-            Text("매운 음식")
+            Text(viewModel.query)
                 .font(.system(size: 40))
                 .fontWeight(.black)
             HStack {
-                ZStack {
-                    Circle()
-                        .fill(Color.blue)
-                        .frame(width: 130, height: 130)
-                    Text("YES")
-                        .font(.system(size: 30))
-                        .foregroundColor(Color.white)
-                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                }
+                Button(action: { viewModel.onYes() }, label: {
+                    ZStack {
+
+                        Circle()
+                            .fill(Color.blue)
+                            .frame(width: 130, height: 130)
+                        Text("YES")
+                            .font(.system(size: 30))
+                            .foregroundColor(Color.white)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    }
+                })
+
                 Divider().frame(height: 180).padding(.horizontal, 20)
 
-                ZStack {
-                    Circle()
-                        .fill(Color.orange)
-                        .frame(width: 130, height: 130)
-                    Text("NO")
-                        .font(.system(size: 30))
-                        .foregroundColor(Color.white)
-                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                }
+                Button(action: { viewModel.onNo() }, label: {
+                    ZStack {
+                        Circle()
+                            .fill(Color.orange)
+                            .frame(width: 130, height: 130)
+                        Text("No")
+                            .font(.system(size: 30))
+                            .foregroundColor(Color.white)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    }
+                })
 
             }
         }
