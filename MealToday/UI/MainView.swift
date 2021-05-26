3b6aff86471758f1d91b25c1c36135209add5a43
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2021-05-26 23:21:36 +0900

    [#12] Update MainView.swift

--- MealToday/UI/MainView.swift
+++ MealToday/UI/MainView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 struct MainView: View {
-    @EnvironmentObject var appState: AppState
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
