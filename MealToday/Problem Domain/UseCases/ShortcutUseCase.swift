4ac3a0c09dc16762d0e9c6993df94991976c0fd7
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2021-06-01 17:30:08 +0900

    [#16] Update ShortcutUseCase.swift

--- MealToday/Problem Domain/UseCases/ShortcutUseCase.swift
+++ MealToday/Problem Domain/UseCases/ShortcutUseCase.swift
@@ -10,20 +10,62 @@ import Combine
 
 protocol ShortcutUseCase {
     func getShortcuts() -> AnyPublisher<[Shortcut], Never>
-    func createShortcut(name: String, selections: Selections)
+    func addShortcut(name: String, selections: Selections, icon: String, color: CodableColor)
+    func removeShorcut(id: Shortcut.ID)
+}
+
+extension ShortcutUseCase {
+    func addShortcut(name: String, selections: Selections, icon: String = Shortcut.defaultIcon, color: CodableColor = .random) {
+        addShortcut(name: name, selections: selections, icon: icon, color: color)
+    }
 }
 
 #if DEBUG
-typealias DefaultShortcutUseCase = StubShortcutUseCase
-
 struct StubShortcutUseCase: ShortcutUseCase {
-    func getShortcuts() -> AnyPublisher<[Shortcut], Never> {
-        Just(Shortcut.stub).eraseToAnyPublisher()
-    }
-
-    func createShortcut(name: String, selections: [FoodType: Bool]) {
+    func removeShorcut(id: Shortcut.ID) {
         //
     }
 
+    func getShortcuts() -> AnyPublisher<[Shortcut], Never> {
+        Just(Shortcut.stub).eraseToAnyPublisher()
+    }
 }
 #endif
+
+class DefaultShortcutUseCase: ShortcutUseCase {
+
+    @Published private var shortcutStore: ShortcutStore
+
+    init() {
+        let defaultKey = "ShortcutStore"
+        shortcutStore = ShortcutStore(json: UserDefaults.standard.data(forKey: defaultKey)) ?? ShortcutStore()
+
+        $shortcutStore.sink { UserDefaults.standard.set($0.json, forKey: defaultKey) }
+            .store(in: &cancelBag)
+
+    }
+
+    func getShortcuts() -> AnyPublisher<[Shortcut], Never> {
+        $shortcutStore.map { $0.shortcuts }.eraseToAnyPublisher()
+    }
+
+    func addShortcut(name: String, selections: Selections, icon: String, color: CodableColor) {
+        var description = selections.filter { $0.value == true }.keys.reduce("", { $0 + $1.rawValue + ", "})
+        _ = description.popLast()
+        _ = description.popLast()
+        shortcutStore.addShortcut(
+            name: name,
+            description: description,
+            selections: selections,
+            icon: icon,
+            color: color
+        )
+    }
+
+    func removeShorcut(id: Shortcut.ID) {
+        shortcutStore.removeShortcut(id: id)
+    }
+
+    // MARK: - Private
+    private var cancelBag = Set<AnyCancellable>()
+}
