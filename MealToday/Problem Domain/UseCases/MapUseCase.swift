870444599ae5e0da53bcafb22705dcaec23283d1
krwns97 <krwns97@gmail.com>
2021-06-01 18:01:38 +0900

    [#17] Update MapUseCase.swift

--- MealToday/Problem Domain/UseCases/MapUseCase.swift
+++ MealToday/Problem Domain/UseCases/MapUseCase.swift
@@ -6,21 +6,44 @@
 //
 
 import Foundation
+import UIKit
 
 protocol MapUseCase: UseCase {
-    func routeToMap(with: Food, currentLocation: Location)
+    func routeToMap(with: Food)
 }
 
 #if DEBUG
 struct StubMapUseCase: MapUseCase {
-    func routeToMap(with: Food, currentLocation: Location) {
-        //
+    func routeToMap(with: Food) {
+
+        let string = "https://m.map.kakao.com/actions/searchView?q="+"치킨"
+        guard let string2 = string.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { return }
+        guard let url=URL(string: string2) else {
+            print("error : URL is nil")
+            return }
+
+        if UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+            print("error : cannot open safari or kakaomap")
+        }
     }
 }
 #endif
 
 struct DefaultMapUseCase: MapUseCase {
-    func routeToMap(with: Food, currentLocation: Location) {
+    func routeToMap(with: Food) {
         //
+        let string = "https://m.map.kakao.com/actions/searchView?q="+with.name
+        guard let string2 = string.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { return }
+        guard let url=URL(string: string2) else {
+            print("error : URL is nil")
+            return }
+
+        if UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+            print("error : cannot open safari or kakaomap")
+        }
     }
 }
