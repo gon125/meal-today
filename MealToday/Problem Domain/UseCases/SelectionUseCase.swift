e016da2dad4019a18c418df9ff44d79624d41850
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2021-06-09 22:17:46 +0900

    [#23] Update SelectionUseCase.swift

--- MealToday/Problem Domain/UseCases/SelectionUseCase.swift
+++ MealToday/Problem Domain/UseCases/SelectionUseCase.swift
@@ -14,9 +14,7 @@ protocol SelectionUseCase: UseCase {
 }
 
 #if DEBUG
-typealias DefaultSelectionUseCase = StubSelectionUseCaase
-
-struct StubSelectionUseCaase: SelectionUseCase {
+struct StubSelectionUseCase: SelectionUseCase {
 
     private var selections = Selections()
     private var queries: [FoodType] = [.dessertOrMeal]
@@ -43,3 +41,65 @@ struct StubSelectionUseCaase: SelectionUseCase {
 
 }
 #endif
+
+typealias DefaultSelectionUseCase = FoodSelectionUseCase
+
+class FoodSelectionUseCase: SelectionUseCase {
+
+    private var selections = Selections()
+    private var queries: [FoodType] = [.dessertOrMeal]
+    private let repository: FoodRepository
+    var foods: LoopIterator<Food>?
+    var selectedFoodIndex = 0
+
+    init(repository: FoodRepository) {
+        self.repository = repository
+    }
+
+    func getFoodFromSelection() -> AnyPublisher<Food?, Never> {
+        if foods != nil { return Just(foods!.next()).eraseToAnyPublisher() }
+        return repository.getFood(from: selections).map { [weak self] in
+            self?.foods = $0?.shuffled().makeIterator()
+            return self?.foods?.next()
+        }
+        .eraseToAnyPublisher()
+    }
+
+    func addChoice(for foodType: FoodType, isChosen: Bool) {
+        if foodType == .dessertOrMeal {
+            queries += isChosen ? FoodType.dessert : FoodType.meal
+        } else {
+            selections[foodType] = isChosen
+        }
+    }
+
+    func getNextQuery() -> AnyPublisher<FoodType?, Never> {
+        guard let query = queries.popLast() else { return Just(nil).eraseToAnyPublisher() }
+        return Just(query).eraseToAnyPublisher()
+    }
+
+    func setSelections(with selections: Selections) {
+        self.selections = selections
+    }
+}
+
+struct LoopIterator<T>: IteratorProtocol {
+    private var elements: [T]
+    private var index = 0
+
+    init(_ elements: [T]) {
+        self.elements = elements
+    }
+
+    mutating func next() -> T? {
+        guard !elements.isEmpty else { return nil }
+        index = (index + 1) % elements.count
+        return elements[index]
+    }
+}
+
+extension Array where Element == Food {
+    fileprivate func makeIterator() -> LoopIterator<Food> {
+        LoopIterator(self)
+    }
+}
