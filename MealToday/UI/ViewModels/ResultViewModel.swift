85e9ab8a9a22ea60b8b2561048305b67d5890321
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2021-06-10 23:56:45 +0900

    [#27] Update ResultViewModel.swift

--- MealToday/UI/ViewModels/ResultViewModel.swift
+++ MealToday/UI/ViewModels/ResultViewModel.swift
@@ -11,8 +11,11 @@ import Combine
 extension ResultView {
     final class ViewModel: ObservableObject {
         @Published var foodName: String = ""
+        var food: Food?
         @Injected private var selectionUseCase: SelectionUseCase
         @Injected private var shortcutUseCase: ShortcutUseCase
+        @Injected private var appState: AppState
+        @Injected private var mapUseCase: MapUseCase
 
         init() {
             selectFood()
@@ -25,8 +28,25 @@ extension ResultView {
         private var cancelBag = Set<AnyCancellable>()
         private func selectFood() {
             selectionUseCase.getFoodFromSelection().sink { [weak self] in
+                self?.food = $0
                 self?.foodName = $0?.name ?? "없습니다\nㅜㅡㅠ"
             }.store(in: &cancelBag)
+        }
+
+        func routeToShortcutView() {
+            selectionUseCase.reset()
+            appState.viewRouting = .shortcut
+        }
+
+        func saveShortcut(name: String, icon: String, color: CodableColor) {
+            shortcutUseCase.addShortcut(name: name, selections: selectionUseCase.getSelections(), icon: icon, color: color)
+        }
+
+        func routeToMap() {
+            guard let food = food else {
+                return
+            }
+            mapUseCase.routeToMap(with: food)
         }
     }
 }
